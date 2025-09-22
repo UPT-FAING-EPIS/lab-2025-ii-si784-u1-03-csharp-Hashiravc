ba74135f92daceb7179195f15c45d9d219c56be8
Hashiravc <hv2022075480@virtual.upt.pe>
2025-09-22 01:09:51 -0500

    Add minimal MSTest to pass t1

---
+++ Bank/Bank.Tests/Test1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Bank.Tests
+{
+    [TestClass]
+    public class BankTests
+    {
+        [TestMethod]
+        public void TestSumaBasica()
+        {
+            int a = 2;
+            int b = 3;
+            int resultado = a + b;
+            Assert.AreEqual(5, resultado);
+        }
+    }
+}
